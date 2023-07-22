9e9a8b0212aefa05e63764e6d7ab625e3e6842a1
Your Name <youremail@example.com>
2023-07-21 17:29:57 -0700

    REST API for employee ID

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,47 +1,29 @@
 #!/usr/bin/python3
-
+"""
+Python script that uses a REST API
+(https://jsonplaceholder.typicode.com/), for a given
+employee ID, returns information about his/her TODO list progress
+"""
+import json
 import requests
-
-def get_employee_todo_progress(employee_id):
-    base_url = "https://example.com/api/employees"  # Replace with the actual API endpoint URL
-
-    # Get employee information
-    employee_url = f"{base_url}/{employee_id}"
-    response = requests.get(employee_url)
-
-    if response.status_code != 200:
-        print("Failed to fetch employee information.")
-        return
-
-    employee_data = response.json()
-    employee_name = employee_data["name"]
-
-    # Get employee tasks
-    tasks_url = f"{base_url}/{employee_id}/tasks"
-    response = requests.get(tasks_url)
-
-    if response.status_code != 200:
-        print(f"Failed to fetch tasks for Employee {employee_name}.")
-        return
-
-    tasks_data = response.json()
-    total_tasks = len(tasks_data)
-    completed_tasks = [task for task in tasks_data if task["completed"]]
-    num_completed_tasks = len(completed_tasks)
-
-    # Print the employee TODO list progress
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
-
-    # Print the titles of completed tasks
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+import sys
+from sys import argv
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 2:
-        print("Usage: python script.py employee_id")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
-
+        url_users = "https://jsonplaceholder.typicode.com/users/{}"
+        url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"
+        url = requests.get(url_users.format(argv[1]))
+        data_name = url.json().get('name')
+        url = requests.get(url_todos.format(argv[1]))
+        data = url.json()
+        done = 0
+        total = 0
+        for task in data:
+                total += 1
+                if task.get("completed"):
+                        done += 1
+        first_line = "Employee {} is done with tasks({}/{}):"
+        print(first_line.format(data_name, done, total))
+        for task in data:
+                if task.get("completed"):
+                        print("\t {}".format(task.get("title")))
